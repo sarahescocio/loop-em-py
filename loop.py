885cc662a661926a0a5cc23f3397b050f74f6cdf
Sarah Escócio <114763074+sarahescocio@users.noreply.github.com>
2022-10-26 09:15:05 -0300

    First commit

---
+++ loop.py
@@ -0,0 +1,12 @@
+secret_number = 13
+user_number = int( input('Qual número você imagina que tem o presidente que irá ganhar? ') )
+
+
+while user_number != secret_number: 
+	print('ÉRRRR, se liga, hein! Bora tentar de novo? ')
+	user_number = int( input('Qual número você imagina que tem o presidente que irá ganhar? ') )
+		
+
+print('Parabénssssss, é isso aí, ja diria Ana Carolina! ')
+
+
