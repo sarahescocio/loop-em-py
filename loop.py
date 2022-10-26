01d4cff47c587798aea9944a3115d50fbaac4019
arturo32 <arturofonsecas32@gmail.com>
2022-10-26 09:46:58 -0300

    Improve comments and formatting in code

--- loop.py
+++ loop.py
@@ -1,12 +1,17 @@
+# This program is a game for guessing a secret number
+
+
 secret_number = 13
-user_number = int( input('Qual número você imagina que tem o presidente que irá ganhar? ') )
+user_number = int( input( 'Qual número você imagina que tem o presidente que irá ganhar? ' ) )
 
 
+# Keep asking the number for the user if it isn't the same as the secret_number
 while user_number != secret_number: 
 	print('ÉRRRR, se liga, hein! Bora tentar de novo? ')
-	user_number = int( input('Qual número você imagina que tem o presidente que irá ganhar? ') )
+	user_number = int( input( 'Qual número você imagina que tem o presidente que irá ganhar? ' ) )
 		
 
-print('Parabénssssss, é isso aí, ja diria Ana Carolina! ')
+# If the user finally guess the number, print a congratulations message
+print( 'Parabénssssss, é isso aí, ja diria Ana Carolina!' )
 
 
